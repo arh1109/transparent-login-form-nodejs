a6a3498223c91ebf30109cbb67447cf42e6a5fef
arh1109 <andyhughes39@yahoo.com>
2021-04-21 17:12:06 -0400

    blah

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(express.static(__dirname + '/views'));
 
 
-app.listen(process.env.PORT || '3000', () => {
+app.listen(process.env.PORT || '8080', () => {
     console.log(`Server is running on port ${process.env.PORT || 3000}`);
     
 })
